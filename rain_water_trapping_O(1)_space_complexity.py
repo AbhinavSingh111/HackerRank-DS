a7190075ee9cb12774b4ffc16962ca5e270821d0
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-06-07 14:07:29 +0530

    Create rain_water_trapping_O(1)_space_complexity.py
    This code solves the rain water trapping problem in O(1) space and O(n) time complexity

---
+++ rain_water_trapping_O(1)_space_complexity.py
@@ -0,0 +1,23 @@
+# this approach has O(n) time complexity and O(1) space complexity
+buildings = [3,1,2,4,0,1,3,2]
+l,r=0,len(buildings)-1
+left_largest = 0
+right_largest = 0
+water_trapped = 0
+
+while(l<=r):
+    if buildings[l]<=buildings[r]:
+        if buildings[l]>=left_largest:
+            left_largest=buildings[l]
+            l+=1
+        else:
+            water_trapped+=left_largest-buildings[l]
+            l+=1
+    elif buildings[r]<=buildings[l]:
+        if buildings[r]>=right_largest:
+            right_largest=buildings[r]
+            r-=1
+        else:
+            water_trapped+=right_largest-buildings[r]
+            r-=1
+print(water_trapped)
