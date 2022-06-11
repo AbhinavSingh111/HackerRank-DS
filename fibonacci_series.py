27705d90ce2cf3d170edd0a59a19195f16739058
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-06-11 23:01:10 +0530

    Create fibonacci_series.py

---
+++ fibonacci_series.py
@@ -0,0 +1,21 @@
+def fibonacci_series(n,second_last,last):
+    if n-1==0:
+        return last
+    else:
+        new_last=second_last+last
+        second_last=last
+        return fibonacci_series(n-1,second_last,new_last)
+
+def fibonacci_series_other_method(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci_series_other_method(n-1)+fibonacci_series_other_method(n-2)
+
+if __name__=="__main__":
+    n = 10
+    print(n)
+    print(fibonacci_series(n,0,1))
+    print(fibonacci_series_other_method(n))
