7e7421c2e25966c00188df4cd7a96d91e0521495
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-07-24 16:34:30 +0530

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -13,9 +13,24 @@ def fibonacci_series_other_method(n):
         return 1
     else:
         return fibonacci_series_other_method(n-1)+fibonacci_series_other_method(n-2)
+    
+    
+ def fibonacci_by_dp_memoization(n,bank):
+    if n==1:
+        return 1
+    elif n==0:
+        return 0
+    elif bank[n]!=0:
+        return bank[n]
+    else:
+        print("function called")
+        fib = fibonacci_by_dp_memoization(n-1,bank)+fibonacci_by_dp_memoization(n-2,bank)
+        bank[n]=fib
+        return fib
 
 if __name__=="__main__":
     n = 10
     print(n)
     print(fibonacci_series(n,0,1))
     print(fibonacci_series_other_method(n))
+    print(fibonacci_by_dp_memoization(n,bank))
