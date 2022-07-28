995ccbe7f486840dd3ba7ba748daac145206130f
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-07-28 23:57:04 +0530

    Update hckrank_python_map_lambda_func.py

--- hckrank_python_map_lambda_func.py
+++ hckrank_python_map_lambda_func.py
@@ -18,6 +18,11 @@ def fibonacci(n):
                 i=j
                 j=s
     return l
+#  or
+    lis = [0,1]
+    for i in range(2,n):
+        lis.append(lis[i-2] + lis[i-1])
+    return(lis[0:n])
         
     
 
