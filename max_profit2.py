d4397d4d549ec4ec51d238832c918fb3d452db30
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-08-12 00:06:54 +0530

    Update max_profit2.py

--- max_profit2.py
+++ max_profit2.py
@@ -9,3 +9,22 @@ def maxProfit(prices):
     return max_p
 prices=[3,1,4,8,7,2,5]
 print(maxProfit(prices))
+
+
+# or 
+def maxProfit(prices): #take every trancsaction that gives profit , ie , all byu at all dips and sell at all peaks
+    max_p=0
+    bd=0 #cbuying date
+    sd=0 #selling date
+    for i in range(1,len(prices)):
+        if prices[i]>=prices[i-1]: #checks if the next price is greater , if yes then move sd ahead
+            sd+=1
+        else: #if not then collect the profit and move both buying and sell date
+            max_p+=prices[sd]-prices[bd]
+            bd=i
+            sd=i
+    max_p+=prices[sd]-prices[bd]
+    return max_p
+    
+prices=[3,1,4,8,7,2,5]
+print(maxProfit(prices))
