7cd7ba253f91077ba103da622f55c2c4d82bd9ab
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-08-16 23:23:02 +0530

    Update next_grater_element.py

--- next_grater_element.py
+++ next_grater_element.py
@@ -28,3 +28,30 @@ def nextGreaterElementToRight(nums1, nums2):
 nums1=[4,1,2]
 nums2=[1,3,4,2]
 print(nextGreaterElementToRight(nums1,nums2))
+
+
+# slight variation of same question
+def nextLargerElementToRight(self,arr,n):
+        stack=[]
+        vector=[]
+        for i in range(n-1,-1,-1):
+            if len(stack)==0:
+                vector.append(-1)
+            elif len(stack)!=0 and arr[i]<stack[-1]:
+                vector.append(stack[-1])
+            elif len(stack)!=0 and arr[i]>=stack[-1]:
+                while len(stack)!=0 and arr[i]>=stack[-1]:
+                    stack.pop()
+                if len(stack)==0:
+                    vector.append(-1)
+                else:
+                    vector.append(stack[-1])
+            stack.append(arr[i])
+        return vector[::-1]
+
+n = 4
+arr = [1, 3, 2 ,4]
+print(nextLargerElementToRight(n,arr))
+
+
+# questions like greater to left , smaller to right , smaller to right are variations of this question
