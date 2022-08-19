933b2d350b9d77774ff4d20ee200a0d4d2ca70cb
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-08-20 01:10:31 +0530

    Update min_stack.py

--- min_stack.py
+++ min_stack.py
@@ -39,3 +39,68 @@ def min_stack(arr):
 
 arr = [18,19,29,15,16]
 print(min_stack(arr))
+
+
+
+# using o(1)space complexity
+stack=[]
+min_ele = 0
+
+def push(ele):
+    global min_ele
+    if len(stack)==0 :
+        stack.append(ele)
+        min_ele = ele
+    else:
+        if ele<min_ele:
+            f = (2*ele)-min_ele
+            stack.append(f)
+            min_ele=ele
+        else:
+            stack.append(ele)
+    
+
+def pop():
+    global min_ele
+    if len(stack)==0:
+        return -1
+    else:
+        if stack[-1]<min_ele:
+            min_ele = (2*min_ele)-stack[-1]
+            stack.pop()
+        else:
+            stack.pop()
+    
+
+
+
+def get_top():
+    global min_ele
+    if len(stack)==0:
+        return -1
+    else:
+        if stack[-1]<min_ele:
+            return (2*min_ele)-stack[-1]   
+        else:
+            return stack[-1]
+
+
+def get_min():
+    global min_ele
+    if len(stack)==0:
+        return -1
+    else:
+        return min_ele
+
+def min_stack(arr):
+    push(arr[0])
+    push(arr[1])
+    push(arr[2])
+    push(arr[3])
+    # pop()
+    # pop()
+    # pop()
+    return get_min()
+
+arr = [18,19,29,15,16]
+print(min_stack(arr))
