bf6c596531a3e0fe09f3060ca759f3f63fd3c348
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-09-02 23:39:38 +0530

    Create bitonic_arr_search.py

---
+++ bitonic_arr_search.py
@@ -0,0 +1,88 @@
+#  to search an ele in bitonic array , find the peak , then apply bin search in both arrays 
+# bin_search_asc(arr,ele , 0 , peak-1)
+# bin_search_desc(arr , ele , peak , len(arr)-1)
+
+
+
+# a bitonic array is an array in which the elements first increase continously and then decrease continously
+# a bitonic array can have only one peak , ie an element which is greater than both of its neighbours ,
+# if looked closely , that element will be the pivot , ie the continouity of the array broke , or we can say that
+# pivot/peak is the point from where the array starts to decrease.
+# so we just use either the concept of finding pivot or finding peak
+
+# here we will be solving this by finding peak
+
+
+
+# find the peak element in a given unsorted array
+# here we use the concept of Binary Search on Answer Concept , ie acc to custom conditons for mid ele , and 
+# custom conditions for moving either left or right  , we apply binary search 
+
+
+# here the peak element is the element which is greater than both of its neighbours
+# if mid is on the extreme left ie 0 ,  then it should be greater than element at 1 index , otherwise return ele at 1
+# if mid is on the extreme right  ie size-1 ,  then it should be greater than element at size-1-1 index , otherwise return ele at size-1-1
+
+# this will return only one peak ele , even if there are more than 1
+def peak_ele(arr):
+    start = 0
+    end = len(arr)-1
+    while start<=end:
+        mid = start+(end-start)//2
+        if mid>0 and mid<len(arr)-1:
+            if arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]:
+                return mid
+            elif arr[mid-1]>arr[mid+1]:
+                end = mid-1
+            elif arr[mid+1]>arr[mid-1]:
+                start = mid+1
+        elif mid==0:
+            if arr[mid]>arr[1]:
+                return mid
+            else:
+                return 1
+        elif mid==len(arr)-1:
+            if arr[mid]>len(arr)-2:
+                return mid
+            else:
+                return len(arr)-2
+
+def bin_search_asc(arr , ele , start , end):
+    while start<=end:
+        mid = start+(end-start)//2
+        if arr[mid]==ele:
+            return mid
+        elif ele<arr[mid]:
+            end = mid-1
+        elif ele>arr[mid]:
+            start = mid+1
+    return -1
+
+
+def bin_search_desc(arr , ele ,start , end):
+    while start<=end:
+        mid = start+(end-start)//2
+        if arr[mid]==ele:
+            return mid
+        elif ele<arr[mid]:
+            start = mid+1
+        elif ele>arr[mid]:
+            end = mid-1          
+    return -1
+
+def search_ele_in_bitonic_arr(arr,ele):
+    peak = peak_ele(arr)
+    res1 = bin_search_asc(arr,ele , 0 , peak-1)
+    res2 = bin_search_desc(arr , ele , peak , len(arr)-1)
+    if res1 !=-1:
+        return res1
+    else:
+        return res2
+
+    
+    
+
+
+arr = [10,20,50,55,23,13,7]
+ele = 7
+print(search_ele_in_bitonic_arr(arr,ele))
