d3d9ab15a76fd6dc6007a01910586656a9859982
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-09-27 23:44:39 +0530

    Create K_th_Symbol_in_Grammar.py

---
+++ K_th_Symbol_in_Grammar.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/k-th-symbol-in-grammar/
+class Solution:
+    def kthGrammar(self , n, k):
+        if n==1 and k==1:
+            return 0
+        mid  = 2**(n-1)//2
+        if mid>=k:
+            return self.kthGrammar(n-1, k)
+        if mid<k:
+             return int(not self.kthGrammar(n-1, k-mid))
