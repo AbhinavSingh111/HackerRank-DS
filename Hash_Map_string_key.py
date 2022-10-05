00e79e248af7c70ea4db4f88552ca57c17bbec16
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-05 23:46:14 +0530

    Update Hash_Map_string_key.py

--- Hash_Map_string_key.py
+++ Hash_Map_string_key.py
@@ -1,6 +1,6 @@
 """Write a HashTable class that stores strings
 in a hash table, where keys are calculated
-using the first two letters of the string."""
+using the first two letters of the string.""
 
 class HashTable(object):
     def __init__(self):
