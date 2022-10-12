f8f2e9dfec0d372d72950027df0837872b55c873
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-12 15:34:39 +0530

    Update shortest_commom_supersequence.py

--- shortest_commom_supersequence.py
+++ shortest_commom_supersequence.py
@@ -31,3 +31,53 @@ arr2="acbcf"
 length1 = len(arr1)
 length2 = len(arr2)
 print(shortest_common_supersequence(arr1, arr2 , length1 , length2))
+
+
+
+
+# print_shortest_common_supersequence
+
+def lcs(arr1 , arr2 , length1 , length2):
+    dp = [[-1 for i in range(length2+1)]for j in range(length1+1)]
+    for i in range(length1+1):
+        for j in range(length2+1):
+            if i==0 or j==0:
+                dp[i][j]=0
+            elif arr1[i-1]==arr2[j-1]:
+                dp[i][j] = 1+dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i][j-1] , dp[i-1][j])
+    return dp
+
+def pscs(arr1 , arr2 , length1 , length2):
+    s=""
+    dp = lcs(arr1 , arr2 , length1 , length2)
+    i , j = length1 , length2
+    while i>0 and j>0:
+        if arr1[i-1]==arr2[j-1]:
+            s+=arr1[i-1]
+            i-=1
+            j-=1
+        else:
+            
+            if dp[i][j-1]>dp[i-1][j]:
+                s+=arr2[j-1]
+                j-=1
+            else:
+                s+=arr1[i-1]
+                i-=1
+    while i>0:
+        s+=arr1[i-1]
+        i-=1
+    while j>0:
+        s+=arr2[j-1]
+        j-=1
+    return s[::-1]
+
+# arr1 = "AGGTAB"
+arr1="abcdaf"
+arr2="acbcf"
+# arr2 = "GXTXAYB"
+length1 = len(arr1)
+length2 = len(arr2)
+print(pscs(arr1, arr2 , length1 , length2))
