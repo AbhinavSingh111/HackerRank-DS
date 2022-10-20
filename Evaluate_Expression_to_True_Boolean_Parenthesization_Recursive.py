5f045ae20542f1db79ed91d75aa336545d359e9e
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-20 22:57:52 +0530

    Update Evaluate_Expression_to_True_Boolean_Parenthesization_Recursive.py

--- Evaluate_Expression_to_True_Boolean_Parenthesization_Recursive.py
+++ Evaluate_Expression_to_True_Boolean_Parenthesization_Recursive.py
@@ -45,3 +45,59 @@ val = True
 i = 0
 j = len(s)-1
 print(parenthesization(s , i , j , val ))
+
+
+
+# MEMOIZATION 
+# since in memoization we make dp of those variables who change their value through the course of recursion , 
+# here 3 variables are changing , i , j , val
+# so we will have to make a 3d matrix.
+# to steer from the complex nature of 3d matrix we will use a map/dictionary and stroe the valuse of 
+# i_j_val as a key in the form of string and the count as value
+# and then repeat the same we do with regular 2d dp
+
+dp_map = {}
+def parenthesization_memoized(s , i , j , val):
+    if i>j:
+        return 0
+    if i==j:
+        if s[i]=="T" and val == True:
+            return 1
+        elif s[i] == "F" and val == False:
+            return 1
+        else:
+            return 0
+    temp = s[i]
+    temp = temp+" "+s[j]+" "+str(val)
+    if temp in dp_map:
+        return dp_map[temp]
+    ans = 0
+    for k in range(i+1,j,2):
+        lt = parenthesization_memoized(s , i , k-1  , True)
+        lf = parenthesization_memoized(s , i , k-1  , False)
+        rt = parenthesization_memoized(s , k+1 , j  , True)
+        rf = parenthesization_memoized(s , k+1 , j  , False)
+        if s[k] =="&":
+            if val == True:
+                ans = ans+lt*rt
+            else:
+                ans = ans+lt*rf+lf*rt+lf*rf
+        if s[k]=="|":
+            if val == True:
+                ans = ans+lt*rt+lt*rf+lf*rt
+            else:
+                ans = ans+lf*rf
+        if s[k]=="^":
+            if val == True:
+                ans = ans+lt*rf+lf*rt
+            else:
+                ans = ans+lf*rf+lt*rt
+    dp_map[temp] = ans
+    return ans
+
+
+s = "T^F&T"
+val = True
+i = 0
+j = len(s)-1
+print(parenthesization_memoized(s , i , j , val ))
