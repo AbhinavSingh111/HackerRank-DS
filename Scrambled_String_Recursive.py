528e9e856827962ce46ffd1bde34bee1eb194140
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-20 22:59:01 +0530

    Create Scrambled_String_Recursive.py

---
+++ Scrambled_String_Recursive.py
@@ -0,0 +1,40 @@
+# Scrambled String Recursive
+# a variation of MCM
+# given 2 strings , check if one string is scrambled form of other or not
+# we take strings and start to divide them as MCM , 
+# there can be 2 conditions , if the divided parts have been swapped or not
+# if swapped , compare first part of string 1 with the 2nd part of string 2 and repeat the prochess too check if they are scrampled
+# if not swapped , compare first part of string 1 with 1st part of string 2
+# if either of these conditions return True then we can say that the strings are scrambled
+
+
+def cond1(s1 , s2 , n , k):
+    if(scrambled_strings_recursive(s1[0:k],s2[n-k:n])==True and scrambled_strings_recursive(s1[k:n],s2[0:n-k])==True):
+        return True
+    else:
+        return False
+    
+def cond2(s1 , s2 , n , k):
+    if(scrambled_strings_recursive(s1[0:k],s2[0:k])==True and scrambled_strings_recursive(s1[n-k:n],s2[n-k:n])==True):
+        return True
+    else:
+        return False
+
+def scrambled_strings_recursive(s1 , s2):
+    #  first we will decide the case conditions
+    if s1==s2:
+        return True
+    if len(s1)!=len(s2):
+        return False
+    if len(s1)<=1:
+        return False
+    n = len(s1)
+    ans = False
+    for k in range(1,n):
+        if cond1(s1,s2 , n , k ) or cond2(s1,s2 , n , k):
+            ans = True
+    return ans 
+    
+s1 = "abhinav"
+s2 = "shreyas"
+print(scrambled_strings_recursive(s1 , s2))
