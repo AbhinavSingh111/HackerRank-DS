7d59d016ca47b4b229fb950920a112bfcb51770e
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-22 23:20:00 +0530

    Update Scrambled_String_Recursive.py

--- Scrambled_String_Recursive.py
+++ Scrambled_String_Recursive.py
@@ -38,3 +38,44 @@ def scrambled_strings_recursive(s1 , s2):
 s1 = "abhinav"
 s2 = "shreyas"
 print(scrambled_strings_recursive(s1 , s2))
+
+
+# memmoized version
+
+dp_map = {}
+
+def cond1(s1 , s2 , n , k):
+    if(scrambled_strings_memoized(s1[0:k],s2[n-k:n])==True and scrambled_strings_memoized(s1[k:n],s2[0:n-k])==True):
+        return True
+    else:
+        return False
+    
+def cond2(s1 , s2 , n , k):
+    if(scrambled_strings_memoized(s1[0:k],s2[0:k])==True and scrambled_strings_memoized(s1[n-k:n],s2[n-k:n])==True):
+        return True
+    else:
+        return False
+
+def scrambled_strings_memoized(s1 , s2):
+    #  first we will decide the base conditions
+    if s1==s2:
+        return True
+    if len(s1)!=len(s2):
+        return False
+    if len(s1)<=1:
+        return False
+    temp = s1
+    temp = temp+" "+s2
+    if temp in dp_map:
+        return dp_map[temp]
+    n = len(s1)
+    ans = False
+    for k in range(1,n):
+        if cond1(s1,s2 , n , k ) or cond2(s1,s2 , n , k):
+            ans = True
+    dp_map[temp] = ans
+    return ans 
+    
+s1 = "abhinav"
+s2 = "shreyas_memoized"
+print(scrambled_strings_memoized(s1 , s2))
