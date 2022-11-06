994bde8aac056b16984d9ad14bfb5078f5afeb2f
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-11-06 15:58:34 +0530

    Update product-of-array-except-self.py

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -17,16 +17,28 @@ def productExceptSelf( nums) :
 
 
 #  this approach takes O(n) time
-    right=[1]
-    # N=len(nums)
-    left=1
-    for i in range(len(nums)-1,0,-1):
-        right.append(right[-1]*nums[i])
-    right=right[::-1]
-    for i in range(len(nums)):
-        right[i]=right[i]*left
-        left*=nums[i]
-    return right
-A=[3, 1 ,2 ,5 ,3] 
+#     right=[1]
+#     # N=len(nums)
+#     left=1
+#     for i in range(len(nums)-1,0,-1):
+#         right.append(right[-1]*nums[i])
+#     right=right[::-1]
+#     for i in range(len(nums)):
+#         right[i]=right[i]*left
+#         left*=nums[i]
+#     return right
+ 
 
+        prefix=[1]
+        postfix=[1]
+        res = []
+        for i in range(len(nums)):
+            prefix.append(prefix[-1]*nums[i])
+        for i in range(len(nums)-1,0,-1):
+            prefix[i]= postfix[-1]*nums[i])
+        postfix=postfix[::-1]
+        for i in range(len(nums)):
+            res.append(prefix[i]*postfix[i])
+        return res
+A=[3, 1 ,2 ,5 ,3]
 print(productExceptSelf(A))
