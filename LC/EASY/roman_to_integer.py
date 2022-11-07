b95b8b697bfe288b97e43f0e8ddc567dc510c483
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-11-07 23:52:32 +0530

    Create roman_to_integer.py

---
+++ LC/EASY/roman_to_integer.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/roman-to-integer/
+
+def romanToInt( s):
+    dic = {"I":1,"IV":4,"V":5,"IX":9,"X":10,"XL":40,"L":50,"XC":90,"C":100,"XD":400,"D":500,"CM":900,"M":1000}
+    dic_priority = {"I":1 , "V":2 , "X":3 ,"L":4 ,"C":5 , "D":6 ,"M":7}
+    ans = 0
+    i=0
+    while i < len(s)-1:
+        if dic_priority[s[i]]>=dic_priority[s[i+1]] :
+            ans+= dic[s[i]]
+            i+=1
+        else:
+            ans+= dic[s[i:i+2]]
+            i+=2
+    return ans
+
+s="LVIII"
+print(romanToInt(s))
