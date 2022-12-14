37aea17d59ecd00b47e6a16d003ad195fb55c96c
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-12-14 23:40:54 +0530

    Update jump-game.py

--- LC/EASY/MEDIUM/jump-game.py
+++ LC/EASY/MEDIUM/jump-game.py
@@ -16,26 +16,16 @@ class Solution:
         
 
 
+ITERATIVE APPROACH / GREEDY
 
 
-
-
-
-
-
-
-
-
-
-
-
-        # goal = len(nums) - 1
-        # for i in range(len(nums) - 1 , -1 ,-1):
-        #     # i+nums[i] means that we can take this many jumps or can reach upto this index
-        #     # while goal is the index we want to reach  , so if the condition meets , we shift the goal post with i
-        #     if i+nums[i]>=goal:
-        #         goal = i
-        # return True if(goal==0) else False
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1 , -1 ,-1):
+            # i+nums[i] means that we can take this many jumps or can reach upto this index
+            # while goal is the index we want to reach  , so if the condition meets , we shift the goal post with i
+            if i+nums[i]>=goal:
+                goal = i
+        return True if(goal==0) else False
 
 
         # try the reachability approach and figure out the recursive , memoized and dp soln
