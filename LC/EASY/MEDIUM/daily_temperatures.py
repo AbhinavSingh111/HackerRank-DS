68b07614f44fc7cf245ebc6a52fcb46198da101d
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-12-23 00:23:23 +0530

    Update daily_temperatures.py

--- LC/EASY/MEDIUM/daily_temperatures.py
+++ LC/EASY/MEDIUM/daily_temperatures.py
@@ -43,4 +43,39 @@ class Solution:
             s.append(i)
 
         return reversed(v)
+3: 
+    Here what we are doing is using the constraints given in question to make an array of that size ,
+    we will traverse the temperature array from back to front , while doing so , we will search the arr from the current index of temp + 30+1 till end
+    if we find any values grater that -1 , it will mean that there are values greater than the tempersature[i] that have been enountered earlier while traversing 
+    temperature array
+    we will append those values (which are index of the traversed elements from temperature) in a temporary list j
+    now if after traversing arr we do not find any value larger than -1 it means that there have been no values greater than current temp value yet traversed ,
+    ie len(j)==0 , in that case we will change the value of result array at the particular index(i) to 0
+    
+    else , we will take the min value from j (which will indicate the recent occurance of an element greater than temperature[i] ) and will substract that with the current index
+    to get the difference , and modifty the value in res at current index with the difference 
+    
+    and in last we will modify the arr[t-30] index of arr with current index.
+    
+    
+    def dailyTemperatures(temperatures):
+        arr = [-1 for _ in range(71)]
+        res = [0] * len(temperatures)
+        
+        for i in range(len(temperatures)-1,-1,-1):
+            j = []
+            t = temperatures[i]
+            for x in arr[t-30+1:]:
+                if x > -1:
+                    j.append(x)
+            if len(j) == 0:
+                res[i] = 0
+            else:
+                res[i]=min(j)-i
+            arr[t-30] = i
+
+        return(res)
+
+
+
     
