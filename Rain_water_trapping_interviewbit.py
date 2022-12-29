4fa8dc0f98cf38b17375ea5c1aa752e07d888cf1
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-12-29 23:58:59 +0530

    Update Rain_water_trapping_interviewbit.py

--- Rain_water_trapping_interviewbit.py
+++ Rain_water_trapping_interviewbit.py
@@ -18,3 +18,30 @@ class Solution:
         for i in range(len(self.buildings)):
             water_trapped.append(min(left_largest[i],right_largest[i])-self.buildings[i])
         return sum(water_trapped)
+
+###
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left_largest = []
+        right_largest = []
+
+        for i in range(len(height)):
+            if i==0:
+                left_largest.append(height[i])
+            else:
+                left_largest.append(max(left_largest[-1],height[i]))
+
+
+        for i in range(len(height)-1,-1,-1):
+            if i==len(height)-1:
+                right_largest.append(height[-1])
+            else:
+                right_largest.append(max(right_largest[-1],height[i]))
+        right_largest = right_largest[::-1]
+
+
+        water = 0
+        for i in range(len(height)):
+            water+=(min(left_largest[i],right_largest[i])-height[i])
+        return water
