277511b82786636b1fa6b7221544890c41e0f97f
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-12-30 23:20:15 +0530

    Update generating_balanced_brackets_using_recursion.py

--- generating_balanced_brackets_using_recursion.py
+++ generating_balanced_brackets_using_recursion.py
@@ -20,3 +20,41 @@ open = ip
 close = ip
 print(generate_balanced_brackets(open , close , op))
 
+# USING STACK
+
+https://leetcode.com/problems/generate-parentheses/description/
+22. Generate Parentheses
+
+Intuition
+USING RECURSION
+
+Approach
+only add open parenthesis if open<n
+only add close parenthesis of close<open
+return when open==close==n -->
+
+Complexity
+Time complexity:
+Space complexity:
+Code
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # only add open parenthesis if open<n
+        # only add close parenthesis of close<open
+        # return when open==close==n
+        stack = []
+        res = []
+        def rec(open , close):
+            if open==close==n:
+                res.append("".join(stack))
+            if open<n:
+                stack.append("(")
+                rec(open+1 ,close)
+                stack.pop()
+            if close<open:
+                stack.append(")")
+                rec(open, close+1)
+                stack.pop()
+        rec(0,0)
+        return res
+
