e3cba2a43a6439378e9fc63fc40e8eb0b7771c27
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-12-30 23:29:11 +0530

    Update min_stack.py

--- min_stack.py
+++ min_stack.py
@@ -104,3 +104,71 @@ def min_stack(arr):
 
 arr = [18,19,29,15,16]
 print(min_stack(arr))
+
+
+
+
+
+https://leetcode.com/problems/min-stack/description/
+    155. Min Stack
+
+Intuition
+WE CAN USE VARIABLES TO KEEP THE TIME COMPLEXITY CONSTANT
+
+Approach
+we make a global variable min_ele
+and if the value of current element is less then min_ele then in the stack we append 2*(curr_ele)-min_ele
+and update the value of min_ele with curr_ele
+
+no while popping it will act as a flag , ie whenever in the stack we will encounter a value smaller than the min_ele
+we will have to update the min_ele by using (2*min_ele-stack[-1])
+and then pop the stack
+
+Complexity
+Time complexity:
+Space complexity:
+Code
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+        self.min_ele = None
+        
+
+    def push(self, val: int) -> None:
+        if len(self.s) == 0:
+            self.min_ele=val
+            self.s.append(val)
+        elif val<self.min_ele:
+            self.s.append(2*val-self.min_ele)
+            self.min_ele = val
+        else:
+            self.s.append(val)
+
+        
+
+    def pop(self) -> None:
+        if len(self.s)==0:
+            return
+        elif self.s[-1]<self.min_ele:
+            self.min_ele = 2*self.min_ele-self.s[-1]
+            self.s.pop()
+        else:
+            self.s.pop()
+        
+
+    def top(self) -> int:
+        if len(self.s)==0:
+            return -1
+        elif self.s[-1]<self.min_ele:
+            return self.min_ele
+        else:
+            return self.s[-1]
+        
+
+    def getMin(self) -> int:
+        if len(self.s)==0:
+            return -1
+        else:
+            return self.min_ele
+        
