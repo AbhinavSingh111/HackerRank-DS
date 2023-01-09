59669341e85c5d45fe6373a295c1dba3336e5b46
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2023-01-09 23:58:05 +0530

    Create sort_colors.py

---
+++ LC/EASY/MEDIUM/sort_colors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        ph = []
+        th = []
+        for i in range(len(nums)):
+            if len(ph)==0:
+                ph.append(nums[i])
+            else:
+                if ph[-1]<=nums[i]:
+                    ph.append(nums[i])
+                else:
+                    while len(ph)>0 and nums[i]<ph[-1]:
+                        th.append(ph.pop())
+                    ph.append(nums[i])
+                    while len(th)!=0:
+                        ph.append(th.pop())
