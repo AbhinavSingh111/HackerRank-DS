0f8351a30be0a954381244e3ab9afab0a2470c66
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2023-01-17 23:35:45 +0530

    Create monotone_flip.py

---
+++ LC/EASY/MEDIUM/monotone_flip.py
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/flip-string-to-monotone-increasing/description/
+
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        # ans , one = 0 , 0
+        # for i in s:
+        #     # looking for 1s 
+        #     if i=="1":
+        #         one+=1
+        #     # this will execute only if we have found the first occurence of 1 
+        #     elif one:
+        #         one-=1
+        #         ans+=1
+        # return ans
+        one , zero = 0 , 0
+        for i in s:
+            if i=="1":
+                one+=1
+            elif one:
+                zero+=1
+            zero=min(one,zero)
+        return zero
