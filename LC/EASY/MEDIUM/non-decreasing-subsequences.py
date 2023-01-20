4da23cd507ca3192e9139c6ff5fe3f6ac55f1d4f
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2023-01-20 23:22:33 +0530

    Update non-decreasing-subsequences.py

--- LC/EASY/MEDIUM/non-decreasing-subsequences.py
+++ LC/EASY/MEDIUM/non-decreasing-subsequences.py
@@ -1,1 +1,20 @@
 https://leetcode.com/problems/non-decreasing-subsequences/description/
+491. Non-decreasing Subsequences  
+
+  class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+
+        def BT(i,subsequence):
+            nonlocal res
+            if len(subsequence)>1:
+                res.add(tuple(subsequence))
+            if i==len(nums):
+                return
+            
+            if not subsequence or nums[i] >= subsequence[-1]:
+                BT(i+1, subsequence+[nums[i]]) 
+            BT(i+1, subsequence)
+        
+        BT(0,[])
+        return res
