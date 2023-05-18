35ec65268032cd98b81797abef9401a5ffab6b94
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2023-05-18 17:23:32 +0530

    Create longest_increasing_path.py

---
+++ LC/EASY/MEDIUM/HARD/longest_increasing_path.py
@@ -0,0 +1,27 @@
+329. Longest Increasing Path in a Matrix
+
+https://leetcode.com/problems/longest-increasing-path-in-a-matrix/solutions/3538234/dfs-with-dp-python/
+  
+  
+  class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        row , col = len(matrix) , len(matrix[0])
+        dp = {}
+
+        def dfs(r,c,prv):
+            if r<0 or r==row or c<0 or c==col or matrix[r][c]<=prv:
+                return 0
+            if (r,c) in dp:
+                return dp[(r,c)]
+            res=1
+            res= max(res,1+dfs(r+1,c,matrix[r][c]))
+            res= max(res,1+dfs(r,c+1,matrix[r][c]))
+            res= max(res,1+dfs(r-1,c,matrix[r][c]))
+            res= max(res,1+dfs(r,c-1,matrix[r][c]))
+            dp[(r,c)]=res
+            return res
+    
+        for r,c in itertools.product(range(row),range(col)):
+            dfs(r,c,-1)
+        
+        return max(dp.values())
